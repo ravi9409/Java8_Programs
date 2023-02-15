53bda6ca3296f3659a6f68d905750502fb8efe77
ravi9409 <39340361+ravi9409@users.noreply.github.com>
2023-02-16 01:58:45 +0530

    Update CourseCubeLab1.java

--- CourseCubeLab1.java
+++ CourseCubeLab1.java
@@ -31,5 +31,17 @@ public class CourseCubeLab1 {
         System.out.println("---------------------------------");
         //skip and limit method combined
         list.stream().skip(1).limit(1).forEach(System.out::println);
+        
+          //Three ways to create the stream
+        System.out.println("-----1------");
+        Stream<Integer> myStream1 = Stream.empty();
+        myStream1.forEach(System.out::println);
+        System.out.println("------2------");
+        Stream<Integer> myStream2 = Stream.of(11, 12, 13);
+        myStream2.forEach(System.out::println);
+        List<Integer> numsList = Arrays.asList(11, 12, 13, 14, 15);
+        System.out.println("-----3------");
+        Stream<Integer> myStream3 = numsList.stream();
+        myStream3.forEach(System.out::println);
     }
 }
