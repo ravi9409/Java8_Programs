e7402c90f2b8bdfa4603d1203329f830c7c13882
ravi9409 <39340361+ravi9409@users.noreply.github.com>
2023-02-16 01:38:59 +0530

    Update CourseCubeLab1.java

--- CourseCubeLab1.java
+++ CourseCubeLab1.java
@@ -14,5 +14,22 @@ public class CourseCubeLab1 {
         stream.forEach(System.out::print);
         //You can traverse the stream only once you will get error when you execute line below
         stream.forEach(System.out::print);
+        
+        List<Integer> list
+                = Arrays.asList(0, 2, 4);
+        List<Integer> list1
+                = Arrays.asList(6, 8, 10);
+        List<List<Integer>> list2 = Arrays.asList(list, list1);
+        //method to process flatmap
+        list2.stream().map(Collection::stream).flatMap(e -> e).forEach(System.out::println);
+        //limit method usage
+        System.out.println("---------------------------------");
+        list.stream().limit(2).forEach(System.out::println);
+        System.out.println("---------------------------------");
+        //skip method usage, argument inside skip method starts from 1 and not from 0
+        list.stream().skip(1).forEach(System.out::println);
+        System.out.println("---------------------------------");
+        //skip and limit method combined
+        list.stream().skip(1).limit(1).forEach(System.out::println);
     }
 }
