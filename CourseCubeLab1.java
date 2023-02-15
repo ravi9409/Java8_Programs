6243af25df474b3b5a154317247e2a1929a77536
ravi9409 <39340361+ravi9409@users.noreply.github.com>
2023-02-16 02:19:20 +0530

    Update CourseCubeLab1.java

--- CourseCubeLab1.java
+++ CourseCubeLab1.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-public class CourseCubeLab1 {
+public class StreamClassLab {
     public static void main(String[] args) {
         List<Integer> list= Arrays.asList(1,2,3,4,5,6,7,8,9);
         Stream<Integer> stream=list.stream();
