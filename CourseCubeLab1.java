9adcb1f6e131a6bdd64866e5f7a3596255c5defe
ravi9409 <39340361+ravi9409@users.noreply.github.com>
2023-02-17 01:41:39 +0530

    Update CourseCubeLab1.java

--- CourseCubeLab1.java
+++ CourseCubeLab1.java
@@ -61,5 +61,21 @@ public class StreamClassLab {
         Stream.generate(() -> (new Random()).nextInt(100))
                 .limit(10)
                 .forEach(System.out::println);
+        
+        //Reduce method with exmaple
+        List<Integer> numsList = Arrays.asList(1, 2, 3, 4, 5);
+        int sumResult1 = numsList.stream().reduce(0, (a, b) -> a + b);
+        System.out.println(sumResult1);
+        System.out.println("-------------------");
+        int sumResult2 = numsList.stream().reduce(0, Integer::sum);
+        System.out.println(sumResult2);
+        System.out.println("-------------------");
+        int mulResult = numsList.stream().reduce(1, (a, b) -> a * b);
+        System.out.println(mulResult);
+        System.out.println("-------------------");
+        Integer lastElement = numsList.stream()
+                .reduce((num1, num2) -> num2)
+                .orElse(-1);
+        System.out.println(lastElement);
     }
 }
