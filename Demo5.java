b25d95a634d7e586ce1ca192c123beda8512a5fc
ravi9409 <39340361+ravi9409@users.noreply.github.com>
2024-05-08 23:19:25 +0530

    changes done to make code work

---
+++ Demo5.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+public class Demo5 {
+    public static void main(String[] args) throws IOException {
+      //Stream.of(1,2,3).forEach(System.out::println);
+   //   Stream.generate(()->"hello").forEach(System.out::println);
+       // Files.lines(Paths.get("great.txt")).forEach(System.out::print);
+        /*Predicate<Integer> mytest=Predicate.isEqual()
+        System.out.println(mytest.test(2));*/
+//        List<Integer> list= Arrays.asList(1,2,5,67,8,4,21,4,6,7).stream().filter(predicate).collect(Collectors.toList());
+//        for (int a:list)
+//            System.out.println(a);
+
+    }
+}
